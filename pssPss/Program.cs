42c25351c86bbcbd94908f502b44a4fa521171d9
Jonathan Lyttle <myncijub@gmail.com>
2016-01-05 21:39:40 -0600

    First commit - Plays the entire song

---
+++ pssPss/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pssPss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WMPLib.WindowsMediaPlayer pssPss = new WMPLib.WindowsMediaPlayer();
+            string filePath = Path.GetFullPath("..\\..\\Data\\13 - Pss Pss.mp3");
+            pssPss.URL = filePath;
+            pssPss.controls.play();
+
+            System.Console.WriteLine("*elevator music simulator*");
+            System.Console.ReadKey();
+        }
+        }
+ }
