b2b6d34b3e3f12c9de3e2e56351236f0b3af60ea
Jonathan Lyttle <myncijub@gmail.com>
2016-01-05 22:38:16 -0600

    added play and pause functionality

--- pssPss/Program.cs
+++ pssPss/Program.cs
@@ -11,12 +11,22 @@ namespace pssPss
     {
         static void Main(string[] args)
         {
+            //load mp3 from disk
             WMPLib.WindowsMediaPlayer pssPss = new WMPLib.WindowsMediaPlayer();
+                (pssPss.settings as WMPLib.IWMPSettings).setMode("loop", true);
             string filePath = Path.GetFullPath("..\\..\\Data\\13 - Pss Pss.mp3");
             pssPss.URL = filePath;
+            //set a random amount of time to play it (3-8 seconds)
+            //play
+            //set a random interval to have the program wait
+            //pause
             pssPss.controls.play();
 
             System.Console.WriteLine("*elevator music simulator*");
+            System.Console.ReadKey();
+            pssPss.controls.pause();
+            System.Console.ReadKey();
+            pssPss.controls.play();
             System.Console.ReadKey();
         }
         }
